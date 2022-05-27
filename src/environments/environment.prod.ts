37064d7ee8a7783b7d473a4015bd7b10e468cf57
Jordan Herman <jordanherman1@gmail.com>
2022-05-27 00:02:08 -0400

    setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBlSQltYWxFnWvTrv7ChCgib7pon78G6_8",
+    authDomain: "fir-app-d63a6.firebaseapp.com",
+    databaseURL: "https://fir-app-d63a6-default-rtdb.firebaseio.com",
+    projectId: "fir-app-d63a6",
+    storageBucket: "fir-app-d63a6.appspot.com",
+    messagingSenderId: "135381118222",
+    appId: "1:135381118222:web:b03d64df487f20a5957b5f",
+    measurementId: "G-WPP7TCLF3J"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBlSQltYWxFnWvTrv7ChCgib7pon78G6_8",
+    authDomain: "fir-app-d63a6.firebaseapp.com",
+    databaseURL: "https://fir-app-d63a6-default-rtdb.firebaseio.com",
+    projectId: "fir-app-d63a6",
+    storageBucket: "fir-app-d63a6.appspot.com",
+    messagingSenderId: "135381118222",
+    appId: "1:135381118222:web:b03d64df487f20a5957b5f",
+    measurementId: "G-WPP7TCLF3J"
+  }
 };
 
 /*
