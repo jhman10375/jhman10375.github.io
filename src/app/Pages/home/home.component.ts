99f66b7f125475df52f197e94ba85c8345a8676b
Jordan Herman <jordanherman1@gmail.com>
2022-06-03 03:11:02 -0400

    home page cards made

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -13,6 +13,39 @@ export class HomeComponent implements OnInit {
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`,'',''];
 
+  testText = [
+    {
+      Head:'Who am I?',
+      Body:'<p>My name is Jordan Herman, and I am currently an undergraduate student at the University of Cincinnati. I am currently majoring in computer engineering and minoring in computer science at the College of Engineering and Applied Science. My goal in life is to digitally create the future and bring as many people with me as possible. My action statement is, "The more the Marrier", this statement helps me to seek to meet as many people as possible and educate those I meet on the uses and benefits that the digital age has to offer. Along the way I hope to work on different engineering and design teams to create the future. I picture a future that looks like Marty McFly’s (from Back to the Future) future with hover boards, holograms, flying autonomous cars, etc. My goal with computer engineering is to help innovate the digital world and make access to information even easier than it already is.</p>',
+      HasButton:false,
+      Button:[
+        {
+          link:'',
+          text:''
+        }
+      ]
+    },
+    {
+      Head:'Some things that make me who I am:',
+      Body:'<ol><li>Directing robotics teams for multiple years</li><li>Work at Kroger in various positions</li><li>Leading a chapter in BBYO in multiple positions</li><li>Work at the Jewish Community Center of Greater Columbus in various positions</li></ol>',
+      HasButton:true,
+      Button:[
+        {
+          link:'activities',
+          text:'Activities'
+        },
+        {
+          link:'awards',
+          text:'Awards'
+        },
+        {
+          link:'work',
+          text:'Work'
+        }
+      ]
+    }
+  ];
+
   ngOnInit(): void {
   }
 
