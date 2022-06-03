7487e3874904e5706d3462979c0a5a41712dc84e
Jordan Herman <jordanherman1@gmail.com>
2022-06-03 00:16:15 -0400

    main setup of pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule} from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { WorkComponent } from './Pages/work/work.component';
+import { AwardsComponent } from './Pages/awards/awards.component';
+import { ActivitiesComponent } from './Pages/activities/activities.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent,
+    WorkComponent,
+    AwardsComponent,
+    ActivitiesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
