7061aef0814a220e2f5d7912668a67be43743b45
Jordan Herman <jordanherman1@gmail.com>
2022-06-03 01:27:18 -0400

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivitiesComponent } from './Pages/activities/activities.component';
+import { AwardsComponent } from './Pages/awards/awards.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { WorkComponent } from './Pages/work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: 'home', component: HomeComponent },
+  { path: 'activities', component: ActivitiesComponent },
+  { path: 'awards', component: AwardsComponent },
+  { path: 'work', component: WorkComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,23 @@ import { WorkComponent } from './Pages/work/work.component';
 import { AwardsComponent } from './Pages/awards/awards.component';
 import { ActivitiesComponent } from './Pages/activities/activities.component'
 
+//bootstrap imports
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +50,22 @@ import { ActivitiesComponent } from './Pages/activities/activities.component'
     BrowserAnimationsModule,
     MatButtonModule,
     MatDividerModule,
-    MatCardModule
+    MatCardModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
